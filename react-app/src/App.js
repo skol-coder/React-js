788ee37b0d35f4267d17ac0d4128e629b27f2ae0
skol-coder <sushantkolambe@gmail.com>
2021-06-22 12:44:30 +0530

    class based component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
+class App extends Component{
+  render(){
+    return(
     <div className="App">
-      <h1>Class based component</h1>
+      <h1>class based component</h1>
     </div>
-  );
+    );
+  }
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>function based component</h1>
+//     </div>
+//   );
+// }
 
 export default App;
